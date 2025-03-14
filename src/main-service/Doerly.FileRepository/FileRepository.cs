078e87345c1c6031c8ed35e8d084273ea962fbd0
Arsenii Merkulov <arsenii.vm@gmail.com>
2025-03-15 00:51:57 +0200

    updated FileRepository

--- src/main-service/Doerly.FileRepository/FileRepository.cs
+++ src/main-service/Doerly.FileRepository/FileRepository.cs
@@ -2,6 +2,7 @@ using Azure.Storage.Blobs;
 
 namespace Doerly.FileRepository;
 
+/// <inheritdoc />
 public class FileRepository : IFileRepository
 {
     private readonly BlobServiceClient _blobServiceClient;
@@ -11,22 +12,49 @@ public class FileRepository : IFileRepository
         _blobServiceClient = blobServiceClient;
     }
     
+    public async Task UploadFileAsync(string containerName, string fileName, byte[] fileBytes, Dictionary<string, string> blobTags)
+    {
+        using (var memoryStream = new MemoryStream(fileBytes))
+        {
+            await UploadFileAsync(fileName, containerName, memoryStream, blobTags);
+        }
+    }
+
+    public async Task UploadFileAsync(string containerName, string fileName, byte[] fileBytes)
+    {
+        using (var memoryStream = new MemoryStream(fileBytes))
+        {
+            await UploadFileAsync(fileName, containerName, memoryStream);
+        }
+    }
+
     public async Task UploadFileAsync(string containerName, string fileName, Stream stream)
     {
         var containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
         var blobClient = containerClient.GetBlobClient(fileName);
         await blobClient.UploadAsync(stream, true);
     }
-    
-    public async Task<Stream> DownloadFileAsync(string containerName, string fileName)
+
+    public async Task UploadFileAsync(string containerName, string fileName, Stream stream, Dictionary<string, string> blobTags)
     {
         var containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
         var blobClient = containerClient.GetBlobClient(fileName);
-        var response = await blobClient.DownloadAsync();
-        return response.Value.Content;
+        await blobClient.UploadAsync(stream, true);
+        await blobClient.SetTagsAsync(blobTags);
     }
-    
-    public async Task DeleteFileAsync(string containerName, string fileName)
+
+    public async Task<byte[]> DownloadFileBytesAsync(string containerName, string fileName)
+    {
+        var containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
+        var blobClient = containerClient.GetBlobClient(fileName);
+        using (var memoryStream = new MemoryStream())
+        {
+            await blobClient.DownloadToAsync(memoryStream);
+            return memoryStream.ToArray();
+        }
+    }
+
+    public async Task DeleteFileIfExistsAsync(string containerName, string fileName)
     {
         var containerClient = _blobServiceClient.GetBlobContainerClient(containerName);
         var blobClient = containerClient.GetBlobClient(fileName);
