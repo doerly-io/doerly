2b5579df1997e80267d631cd6aecb860cb3d9e8e
David Dzabakhidze <dzabah.david@gmail.com>
2025-06-05 23:16:14 +0300

    Modified commucation module UI/UX

--- src/doerly-fe/src/app/modules/communication/pages/communication/communication.component.ts
+++ src/doerly-fe/src/app/modules/communication/pages/communication/communication.component.ts
@@ -1,20 +1,47 @@
-import {Component, signal} from '@angular/core';
+import {Component, HostListener, signal} from '@angular/core';
 import {ConversationListComponent} from '../conversation-list/conversation-list.component';
 import {ChatWindowComponent} from '../chat-window/chat-window.component';
+import {ButtonModule} from 'primeng/button';
+import {NgClass, NgIf} from '@angular/common';
+import {TranslateModule} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-communication',
+  standalone: true,
   imports: [
     ConversationListComponent,
-    ChatWindowComponent
+    ChatWindowComponent,
+    ButtonModule,
+    NgIf,
+    NgClass,
+    TranslateModule
   ],
   templateUrl: './communication.component.html',
   styleUrl: './communication.component.scss'
 })
 export class CommunicationComponent {
   protected readonly selectedConversationId = signal<number | undefined>(undefined);
+  protected readonly isMobileView = signal<boolean>(false);
+  private readonly MOBILE_BREAKPOINT = 768;
+
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    this.isMobileView.set(window.innerWidth <= this.MOBILE_BREAKPOINT);
+  }
 
   onConversationSelected(conversationId: number) {
     this.selectedConversationId.set(conversationId);
+  }
+
+  onBackClick() {
+    this.selectedConversationId.set(undefined);
   }
 }
