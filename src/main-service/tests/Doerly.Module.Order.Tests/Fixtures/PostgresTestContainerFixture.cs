cd778b0970f15e7c9946e8f9fba4dc72ffa19358
wher3ver <dddaaannn1322@gmail.com>
2025-06-06 22:05:02 +0300

    Added order tests

---
+++ src/main-service/tests/Doerly.Module.Order.Tests/Fixtures/PostgresTestContainerFixture.cs
@@ -0,0 +1,27 @@
+﻿using Testcontainers.PostgreSql;
+
+namespace Doerly.Module.Order.Tests;
+
+public class PostgresTestContainerFixture : IAsyncLifetime
+{
+    public PostgreSqlContainer Container { get; private set; } = null!;
+    public string ConnectionString => Container.GetConnectionString();
+
+    public async Task InitializeAsync()
+    {
+        Container = new PostgreSqlBuilder()
+            .WithCleanUp(true)
+            .WithImage("postgres:15")
+            .WithDatabase("testdb")
+            .WithUsername("testuser")
+            .WithPassword("testpass")
+            .Build();
+
+        await Container.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Container.StopAsync();
+    }
+}
---
+++ src/main-service/tests/Doerly.Module.Order.Tests/UpdateOrderHandlerTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Doerly.Module.Order.Domain.Handlers.Order;
+using Doerly.Module.Order.Contracts.Dtos;
+using Moq;
+using Doerly.Module.Order.Domain.Handlers;
+using Doerly.Domain;
+using Doerly.FileRepository;
+
+namespace Doerly.Module.Order.Tests;
+public class UpdateOrderHandlerTests : BaseOrderTests
+{
+    private readonly UpdateOrderHandler _handler;
+    private readonly Mock<IDoerlyRequestContext> _doerlyRequestContextMock;
+
+    public UpdateOrderHandlerTests(PostgresTestContainerFixture fixture) : base(fixture)
+    {
+        _doerlyRequestContextMock = new Mock<IDoerlyRequestContext>();
+        var fileRepositoryMock = new Mock<IFileRepository>();
+
+        _handler = new UpdateOrderHandler(OrderDbContext, _doerlyRequestContextMock.Object, fileRepositoryMock.Object);
+    }
+
+    [Fact]
+    public async Task HandleAsync_ShouldUpdateOrder_WhenRequestIsValid()
+    {
+        var order = await AddTestOrderAsync();
+
+        _doerlyRequestContextMock.SetupProperty(x => x.UserId, order.CustomerId);   
+
+        var request = new UpdateOrderRequest
+        {
+            Name = "Updated Name",
+            Description = "Updated Description",
+            Price = order.Price + 10,
+            CategoryId = order.CategoryId,
+            PaymentKind = order.PaymentKind,
+            DueDate = order.DueDate.AddDays(1),
+            IsPriceNegotiable = !order.IsPriceNegotiable
+        };
+
+        var result = await _handler.HandleAsync(order.Id, request, [], []);
+
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task HandleAsync_ShouldReturnFailure_WhenOrderDoesNotExist()
+    {
+        var request = new UpdateOrderRequest
+        {
+            Name = "Name",
+            Description = "Desc"
+        };
+        var result = await _handler.HandleAsync(-1, request, [], []);
+        Assert.False(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task HandleAsync_ShouldReturnFailure_WhenUserIsUnauthorized()
+    {
+        var order = await AddTestOrderAsync();
+
+        var request = new UpdateOrderRequest
+        {
+            Name = "Updated Name",
+            Description = "Updated Description",
+            Price = order.Price + 10,
+            CategoryId = order.CategoryId,
+            PaymentKind = order.PaymentKind,
+            DueDate = order.DueDate.AddDays(1),
+            IsPriceNegotiable = !order.IsPriceNegotiable
+        };
+
+        var result = await _handler.HandleAsync(order.Id, request, [], []);
+        Assert.False(result.IsSuccess);
+    }
+}
